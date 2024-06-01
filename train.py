d85f1d57dc40eee9d3743d51e5032484a22322ef
adityagoel <agoel25@student.ubc.ca>
2024-05-31 17:51:59 -0700

    model initializtion added in train.py

--- train.py
+++ train.py
@@ -4,18 +4,18 @@
 # NOTE: The text is assumed to be pre-tokenized
 
 import os
-import time
-import math
 from contextlib import nullcontext
+import pickle
 
 import numpy as np
 import torch
-import wandb
+from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 
-# ----------------------------------------------------------------------------------------------------------------------------------- #
 # default config values designed to train the model on the OpenWebText dataset, change if running on some other dataset if required
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+# housekeeping related
 out_dir = 'out'
 eval_interval = 2000
 eval_iters = 200
@@ -23,19 +23,30 @@ log_interval = 1
 eval_only = False # if true, exit after the first iteration itself
 init_from = 'start' # start, resume or gpt2 (for pretrained checkpoints)
 
+# data related
 dataset = 'openwebtext' # name of data directory in ./data folder to be used for training
 gradient_accumulation_steps = 5 * 8 # we want to accumulate gradients accross mini batches before the backward pass
 batch_size = 12
 block_size = 1024
 
+# model related
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+bias = False # do we use bias inside LayerNorm and Linear layers?
+
+# pytorch related
 device = 'cuda' # 'cpu', 'cuda' or (for macbooks) 'mps'
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
+backend = 'nccl' # distributed data parallel settings, example: nccl, gloo
 # ----------------------------------------------------------------------------------------------------------------------------------- #
 
 # environment and i/o setup
 # NOTE: ddp is a method in PyTorch for parallelizing model training accress multiple GPUs and nodes, each setup has a rank
 ddp = int(os.environ.get('RANK', -1)) != -1 # get the RANK env variable to see if this is a ddp environment 
 if ddp:
+    init_process_group(backend=backend)
     ddp_rank = int(os.environ['RANK'])
     ddp_local_rank = int(os.environ['LOCAL_RANK']) # rank of the process on the local machine/node
     ddp_world_size = int(os.environ['WORLD_SIZE']) # total number of processes participating in the training
@@ -85,6 +96,62 @@ def get_batch(split):
     else:
         x, y = x.to(device), y.to(device)
     return x, y
+
+# variables for checkpoints to keep track of
+iter_num = 0
+best_val_loss = 1e9
+
+# derive vocab_size from the metadata file
+meta_path = os.path.join(data_dir, 'meta.pkl')
+meta_vocab_size = None
+if os.path.exists(meta_path):
+    with open(meta_path, 'rb') as f:
+        meta = pickle.load(f)
+    meta_vocab_size = meta['vocab_size']
+    print(f"Found vocab_size is {meta_vocab_size} at path {meta_path}")
+
+model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size, bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
+
+# model initialization according to the 'init_from' variable
+if init_from == 'start':
+    print("Initializing the model from start")
+    if meta_vocab_size is None:
+        print("No specific vocab_size found, falling back to GPT-2's 50304")
+        model_args['vocab_size'] = 50304
+    else:
+        model_args['vocab_size'] = meta_vocab_size
+    # create the model
+    gptconfig = GPTConfig(**model_args)
+    model = GPT(gptconfig) # we have a new GPT-2 model built from scratch :)
+elif init_from == 'resume':
+    print(f"Resuming training from {out_dir}")
+    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    checkpoint_model_args = checkpoint['model_args']
+    # load mandatory model arguments from the checkpoint arguments
+    for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
+        model_args[k] = checkpoint_model_args[k]
+    # create the model
+    gptconfif = GPTConfig(**model_args)
+    model = GPT(gptconfif)
+    state_dict = checkpoint['model']
+    # checkpoints state dictionaries sometimes get an unwated prefix, we have to remove it for model.py to work as expected
+    unwanted_prefix = '_orig_mod.'
+    for k,v in list(state_dict.items()):
+        if k.startswith(unwanted_prefix):
+            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+    model.load_state_dict(state_dict)
+    iter_num = checkpoint['iter_num']
+    best_val_loss = checkpoint['best_val_loss']
+elif init_from.startswith('gpt2'):
+    print(f"Initializing the model from OpenAI's GPT-2 weights: {init_from}")
+    # initialize gpt2 args
+    override_args = dict(dropout=dropout) # override the dropout to user input
+    # create the model
+    model = GPT.from_pretrained(init_from, override_args)
+    # maintain model_args dictionary 
+    for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
+        model_args[k] = getattr(model.config, k)
 
 @torch.no_grad()
 def estimate_loss():
