bc368b51a59d6125f4a98c91f843c2dfdc388d5b
adityagoel <agoel25@student.ubc.ca>
2024-06-11 20:37:03 -0700

    added multi headed attention

--- model_trivialised.py
+++ model_trivialised.py
@@ -1,7 +1,36 @@
 from dataclasses import dataclass
+import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # masking as done by openAI
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+    
+    def forward(self, input):
+        B, T, C = input.size() # B = batch size, T = context length, C = m_embd (embedding dimensionality)
+        q, k, v  = self.c_attn(input).split(self.n_embd, dim=2) # calculate query, key and value for all heads
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        w = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        w = w.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf')) # makes sure context for a token is just the tokens before it
+        w = F.softmax(w, dim=-1) # normalizes the attention
+        output = w @ v # back to original shape
+        output = output.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        output = self.residual_dropout(self.c_proj(output))
+        return output
 
 class MLP(nn.Module):
     def __init__(self, config):
