d7ab9335ce373ef64438d35b6b3c1250341bf2eb
adityagoel <agoel25@student.ubc.ca>
2024-06-11 20:32:15 -0700

    trivialised model left with multi headed attention

--- model_trivialised.py
+++ model_trivialised.py
@@ -3,6 +3,32 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+class MLP(nn.Module):
+    def __init__(self, config):
+        super.__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh') # using tanh as approximator since GPT2 used it 
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, input):
+        input = self.c_fc(input)
+        input = self.gelu(input)
+        input = self.c_proj(input)
+        return input
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super.__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 @dataclass
 class GPTConfig:
     block_size: int = 1024
